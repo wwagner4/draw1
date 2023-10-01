b5c1eccde24b5a6087132c1ca21e4ee178ad043b
wwagner4 <atnebel2323>
2023-10-01 22:46:17 +0200

    first animation

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,25 @@ use speedy2d::dimen::{Vec2, Vector2};
 use speedy2d::window::{WindowHandler, WindowHelper};
 use speedy2d::{Graphics2D, Window};
 use std::{thread, time::Duration};
+use itertools::Itertools;
 
+#[derive(Clone, Debug)]
 struct Obj {
     pos: Vec2,
     dir: Vec2,
+}
+
+fn create_ran_obj() -> Obj {
+    let mut rng = rand::thread_rng();
+    let pos = Vector2 {
+        x: rng.gen::<f32>() * 1000.0,
+        y: rng.gen::<f32>() * 1000.0,
+    };
+    let dir = Vector2 {
+        x: rng.gen::<f32>() * 10.0 - 5.0,
+        y: rng.gen::<f32>() * 10.0 - 5.0,
+    };
+    Obj { pos, dir }
 }
 
 fn mv(obj: &Obj) -> Obj {
@@ -18,32 +33,37 @@ fn mv(obj: &Obj) -> Obj {
     }
 }
 
-fn create_ran_obj() -> Obj {
-    let mut rng = rand::thread_rng();
-    let pos = Vector2 {
-        x: rng.gen::<f32>() * 100.0,
-        y: rng.gen::<f32>() * 100.0,
-    };
-    let dir = Vector2 {
-        x: rng.gen::<f32>() * 10.0,
-        y: rng.gen::<f32>() * 10.0,
-    };
-    Obj { pos, dir }
-}
-
 fn main() {
     let window = Window::new_centered("Title", (640, 480)).unwrap();
-    window.run_loop(MyWindowHandler {});
+    let h = MyWindowHandler{
+        objs : (0..200).map(|_| create_ran_obj()).collect_vec(),
+    };
+    window.run_loop(h);
 }
 
-struct MyWindowHandler {}
+
+
+struct MyWindowHandler {
+    objs : Vec<Obj>,
+
+}
 
 impl WindowHandler for MyWindowHandler {
+
     fn on_draw(&mut self, helper: &mut WindowHelper, graphics: &mut Graphics2D) {
+
         graphics.clear_screen(Color::from_rgb(0.8, 0.9, 1.0));
-        graphics.draw_circle((100.0, 100.0), 75.0, Color::BLUE);
-        graphics.draw_line((0.0, 0.0), (50.0, 50.0), 12.0, Color::GREEN);
+
+        for o in &self.objs {
+            graphics.draw_circle((o.pos.x, o.pos.y), 10.0, Color::BLUE);
+        }
+
         thread::sleep(Duration::from_millis(50));
         helper.request_redraw();
+
+        self.objs = self.objs.iter()
+            .map(|o| mv(o))
+            .collect_vec();
+
     }
 }
